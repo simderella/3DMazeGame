c6ecfc3737b428663add149cdf268c019373c669
gimmuan <wls15891@gmail.com>
2024-01-22 14:23:56 +0900

    [Modify]

--- Assets/01.Scripts/Anjinho/Scripts/SceneChange.cs
+++ Assets/01.Scripts/Anjinho/Scripts/SceneChange.cs
@@ -9,28 +9,34 @@ public class SceneChange : MonoBehaviour
     public void MainScene()
     {
         LoadSceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
     }
     public void Stage1Change()
     {
-        LoadSceneManager.LoadScene(1); 
+        LoadSceneManager.LoadScene(1);
+        Time.timeScale = 1.0f;
     }
 
     public void Stage2Change()
     {
         LoadSceneManager.LoadScene(2);
+        Time.timeScale = 1.0f;
     }
 
     public void Stage3Change()
     {
         LoadSceneManager.LoadScene(3);
+        Time.timeScale = 1.0f;
     }
     public void Stage4Change()
     {
         LoadSceneManager.LoadScene(4);
+        Time.timeScale = 1.0f;
     }
     public void Stage5Change()
     {
         LoadSceneManager.LoadScene(6);
+        Time.timeScale = 1.0f;
     }
 
 
