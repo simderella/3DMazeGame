68ae782d04d7af3224b1936819c45e846a3fc381
chamgilom <149364808+chamgilom@users.noreply.github.com>
2024-01-23 20:22:01 +0900

    [fix] itemholder location

--- Assets/02.ScriptableObject/Scripts/ItemClass.cs
+++ Assets/02.ScriptableObject/Scripts/ItemClass.cs
@@ -25,6 +25,9 @@ public class ItemClass : ScriptableObject
             item.GetComponent<BoxCollider>().enabled = false;//아이템의 BoxCollider를 끈다
             item.transform.GetChild(2).GetComponent<BoxCollider>().enabled = false;//아이템의 3번쨰 자식 오브젝트의 BoxCollider를 끈다
             item.GetComponent<Rigidbody>().useGravity = false;
+            Rigidbody rb = item.GetComponent<Rigidbody>();
+            rb.useGravity = false;//중력을 끈다
+            rb.constraints = RigidbodyConstraints.FreezePosition;
             item.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);//생성된 item의 크기를 0.2로 줄인다.
             item.transform.parent = itemHolder.transform;//item을 ItemHolder의 자식으로 생성한다.
         }
