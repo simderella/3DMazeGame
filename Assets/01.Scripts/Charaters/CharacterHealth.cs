8898f8e6f634ab87ba2206b2f3854bbf65220fbd
gimmuan <wls15891@gmail.com>
2024-01-26 16:49:34 +0900

    [fead] PlayerHealthUI

--- Assets/01.Scripts/Charaters/CharacterHealth.cs
+++ Assets/01.Scripts/Charaters/CharacterHealth.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterHealth : MonoBehaviour
 {
@@ -9,21 +10,40 @@ public class CharacterHealth : MonoBehaviour
     private int health;
     public event Action OnDie;
 
+    [SerializeField] private Image healthBarImage; // 이미지 컴포넌트를 저장할 변수
     public bool IsDead => health == 0;
 
     private void Start()
     {
         health = maxHealth;
+
+        // Start 메서드에서 이미지 컴포넌트를 초기화합니다.
+        if (healthBarImage == null)
+        {
+            healthBarImage = GetComponentInChildren<Image>(); // 이미지 컴포넌트가 자식 객체에 있는 경우를 가정하고 가져옵니다.
+        }
+
+        UpdateHealthBar();
     }
 
     public void TakeDamage(int damage)
     {
         if (health == 0) return;
         health = Mathf.Max(health - damage, 0);
-
+        UpdateHealthBar();
         if (health == 0)
             OnDie?.Invoke();
 
         Debug.Log(health);
+    }
+
+    private void UpdateHealthBar()
+    {
+        // healthBarImage가 null이 아니라면 (정상적으로 참조되었다면)
+        if (healthBarImage != null)
+        {
+            // health를 0에서 1 사이의 값으로 정규화하여 fillAmount에 적용
+            healthBarImage.fillAmount = (float)health / maxHealth;
+        }
     }
 }
