9c0c7ea8362b803ed3b1d9f4c7b4b9c09f17ac1d
chamgilom <149364808+chamgilom@users.noreply.github.com>
2024-02-15 16:09:47 +0900

    [fix]compass overlap error

--- Assets/02.ScriptableObject/ItemClassScripts/SpeedItemClass.cs
+++ Assets/02.ScriptableObject/ItemClassScripts/SpeedItemClass.cs
@@ -15,10 +15,10 @@ public class SpeedItemClass : ItemClass
         {
 
             Transform itemHolder = GameObject.Find("ItemHolder").transform; //Hierarchy에 있는 ItemHolder라는 transform을 찾아온다.
-            GameObject item = GameObject.Instantiate(ItemObject, itemHolder.position, Quaternion.identity); //ItemObject를 ItemHolder위치에 rotation값은 0으로 생성한다.
+            GameObject item = GameObject.Instantiate(ItemObject, itemHolder.position, itemHolder.rotation); //ItemObject를 ItemHolder위치에 rotation값은 0으로 생성한다.
             item.transform.GetChild(3).GetComponent<BoxCollider>().enabled = false;//아이템의 3번쨰 자식 오브젝트의 BoxCollider를 끈다
-            item.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);//생성된 item의 크기를 0.2로 줄인다.
             item.transform.parent = itemHolder.transform;//item을 ItemHolder의 자식으로 생성한다.
+            item.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);//생성된 item의 크기를 0.2로 줄인다.
         }
     }
     public override void Use()
