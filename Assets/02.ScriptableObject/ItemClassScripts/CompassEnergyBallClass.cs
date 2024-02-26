46486e70adb29d9e554e21d8d468147cb504329d
chamgilom <149364808+chamgilom@users.noreply.github.com>
2024-02-26 17:25:07 +0900

    [feat]CompassEnergyBall

---
+++ Assets/02.ScriptableObject/ItemClassScripts/CompassEnergyBallClass.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/CompassEnegyBall")]
+public class CompassEnegyBallClass : ItemClass
+{
+
+    //public override void Use()
+    //{
+    //    // 아이템 사용 시 소리 재생
+    //    SoundManager.Instance.PlayItemUseSound();
+
+    //    Transform usingItem = GameObject.Find("UsingItem").transform;
+    //    GameObject compass = GameObject.Instantiate(ItemObject, usingItem.position, usingItem.rotation);
+    //    compass.GetComponent<BoxCollider>().enabled = false;//아이템의 BoxCollider를 끈다
+    //    compass.transform.GetChild(2).GetComponent<BoxCollider>().enabled = false;//아이템의 3번쨰 자식 오브젝트의 BoxCollider를 끈다
+    //    compass.transform.SetParent(usingItem.transform);
+    //    compass.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);//생성된 item의 크기를 0.4로 줄인다.
+    //    Compass compassScript = compass.GetComponent<Compass>();
+    //    Rigidbody rb = compass.GetComponent<Rigidbody>();
+    //    rb.useGravity = false;//중력을 끈다
+    //    rb.constraints = RigidbodyConstraints.FreezePosition;
+    //    if (compassScript.useCompass == false)
+    //    {
+    //        compassScript.useCompass = true;
+    //    }
+    //    itemUsed = true;
+    //}
+
+    public override void Use()
+    {
+        SoundManager.Instance.PlayItemUseSound();
+        CompassEnegyBall compassScript = item.GetComponent<CompassEnegyBall>();
+
+        if (compassScript.useCompass == false)
+        {
+            compassScript.useCompass = true;
+            itemUsed = true;
+            compassUsed = true;
+        }
+        else
+        {
+            itemUsed = false;
+            compassUsed = false;
+        }
+    }
+}
